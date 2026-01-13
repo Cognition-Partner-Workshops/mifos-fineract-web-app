f1e4e30b5b3920454978f2fcff2af2aceba965dc
Brian Smitches <smitheitches@gmail.com>
2026-01-13 21:55:45 +0000

    build updates

--- src/environments/.env.ts
+++ src/environments/.env.ts
@@ -2,8 +2,8 @@
 /* tslint:disable */
 export default {
   'mifos_x': {
-    'version': '260108',
-    'hash': 'g5af11fe75'
+    'version': '260113',
+    'hash': '56a965e8'
   },
   'allow_switching_backend_instance': true
 };
